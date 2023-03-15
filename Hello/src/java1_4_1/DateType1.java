48105564656ee519a61b309f7394cc7b95735096
木村 颯太 <docomado.332@gmail.com>
2023-03-15 21:52:39 +0900

    java 新規作成

---
+++ Hello/src/java1_4_1/DateType1.java
@@ -0,0 +1,19 @@
+package java1_4_1;
+
+public class DateType1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		
+//		問２）
+//		次のプログラムを完成させてください。
+//		String型の name変数 〇〇を自分の名前に変えてください。
+//		System.out.println()を使って「（名前）はjavaを学習しています。」と画面に表示してください。
+		String name = "木村は";
+		String message = "javaを学習しています。";
+		System.out.println(name + message);
+
+	}
+
+}
