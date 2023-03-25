01e096ebdfe87f5c8e3037fa59aad79d87b5de38
木村 颯太 <docomado.332@gmail.com>
2023-03-25 22:03:24 +0900

    java 新規作成

---
+++ Hello/src/java1_6/Variable2.java
@@ -0,0 +1,14 @@
+package java1_6;
+
+public class Variable2 {
+	public static void main(String[] args) {
+//		問１）
+//		変数 x を宣言し、「3」を代入して下さい。
+//		変数 x に自己代入を使って「20」を掛け算して下さい。
+//		その後 x の値をコンソールに表示して下さい。
+		
+		int x = 3;
+		System.out.println(x * 20);
+
+	}
+}
