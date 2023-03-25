878514eb0cfb2aff90e90a9dd8d720cc9f15fcf8
木村 颯太 <docomado.332@gmail.com>
2023-03-25 22:19:23 +0900

    java 新規作成

---
+++ Hello/src/java1_6_1/Variable3.java
@@ -0,0 +1,24 @@
+package java1_6_1;
+
+public class Variable3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		問２）
+//		以下のプログラムを作成してください。
+//		①int 型の変数 i に 100 を代入する。
+//		②インクリメント演算子を適用して数値を 1 増やす。
+//		③コンソールに変数 i の値を表示する。
+//		④デクリメント演算子を適用して数値を 1 減らす。
+//		⑤コンソールに変数 i の値を表示する。
+		int i = 100;
+		i++;
+		System.out.println(i);
+		
+		int a = 100;
+		--a;
+		System.out.println(a);
+
+	}
+
+}
