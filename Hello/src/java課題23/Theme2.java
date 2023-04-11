cbaf2f564ef83e24454704df857c18f03a2219a9
木村 颯太 <docomado.332@gmail.com>
2023-04-11 20:49:15 +0900

    課題23　修正

--- Hello/src/java課題23/Theme2.java
+++ Hello/src/java課題23/Theme2.java
@@ -12,12 +12,12 @@ package java課題23;
 
 public class Theme2 {
 	public static void main(String[] args) {
-		Animal animal = new Animal();
-		String a = "動物名：ライオン";
-		animal.setAnimal(a);
-		System.out.println(animal.getAnimal());
+		animalBean animalBean = new animalBean();
+		String a = "動物名：ライオン\n体長：2.1m\n速度：80km/h" ;
+		animalBean.setanimalBean(a);
+		System.out.println(animalBean.getanimalBean());
 		
-		Length length = new Length();
+		/*Length length = new Length();
 		String b = "体長：2.1m";
 		length.setLength(b);
 		System.out.println(length.getLength());
@@ -25,6 +25,6 @@ public class Theme2 {
 		Speed speed = new Speed();
 		String c = "速度：80km/h";
 		speed.setSpeed(c);
-		System.out.println(speed.getSpeed());
+		System.out.println(speed.getSpeed());*/
 	}
 }
---
+++ Hello/src/java課題23/animalBean.java
@@ -0,0 +1,14 @@
+package java課題23;
+
+public class animalBean {
+	
+			private String animalBean;
+			
+			public String getanimalBean() {
+				return animalBean;
+			}
+			
+			public void setanimalBean(String animalBean) {
+				this.animalBean = animalBean;
+			}
+		}
