b6095e04a3a6d7c3db3807db18e48b75fc68e667
木村 颯太 <docomado.332@gmail.com>
2023-04-13 23:05:50 +0900

    課題23　修正

--- Hello/src/java課題23/Theme2.java
+++ Hello/src/java課題23/Theme2.java
@@ -13,8 +13,14 @@ package java課題23;
 public class Theme2 {
 	public static void main(String[] args) {
 		animalBean animalBean = new animalBean();
-		String a = "動物名：ライオン\n体長：2.1m\n速度：80km/h" ;
-		animalBean.setanimalBean(a);
-		System.out.println(animalBean.getanimalBean());
+		String a = "動物名：ライオン" ;
+		String b = "体長：2.1m" ;
+		String c = "速度：80km/h" ;
+		animalBean.setAnimalName(a);
+		animalBean.setSpeed(b);
+		animalBean.setBodyLength(c);
+		System.out.println(animalBean.getAnimalName());
+		System.out.println(animalBean.getSpeed());
+		System.out.println(animalBean.getBodyLength());
 	}
 }
--- Hello/src/java課題23/animalBean.java
+++ Hello/src/java課題23/animalBean.java
@@ -1,15 +1,33 @@
 package java課題23;
 
 public class animalBean {
+		 
 	
-			private String animalBean;
-			
-			public String getanimalBean() {
-				return animalBean;
-			}
-			
-			public void setanimalBean(String animalBean) {
-				this.animalBean = animalBean;
-			}
-		}
+	private String animalName;
+	private String speed;
+	private String bodyLength;
 
+	public String getAnimalName() {
+		return animalName;
+	}
+
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+	
+	public String getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(String speed) {
+		this.speed = speed;
+	}
+	
+	public String getBodyLength() {
+		return  bodyLength;
+	}
+
+	public void setBodyLength(String bodyLength) {
+		this.bodyLength = bodyLength;
+	}
+}
