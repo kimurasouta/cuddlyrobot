fda9dc55c00ed72790f1189278e02020360d44b6
木村 颯太 <docomado.332@gmail.com>
2023-04-16 21:38:14 +0900

    課題23　コメント追加

--- Hello/src/java課題23/Theme2.java
+++ Hello/src/java課題23/Theme2.java
@@ -12,13 +12,21 @@ package java課題23;
 
 public class Theme2 {
 	public static void main(String[] args) {
+		
+		/*animalBeanクラスをインスタンス化*/
 		animalBean animalBean = new animalBean();
+		
+		/*それぞれの変数に文字列を代入*/
 		String a = "動物名：ライオン" ;
 		String b = "体長：2.1m" ;
 		String c = "速度：80km/h" ;
+		
+		/*それぞれのsetterメソッドに変数を渡す*/
 		animalBean.setAnimalName(a);
 		animalBean.setSpeed(b);
 		animalBean.setBodyLength(c);
+		
+		/*getterメソッドでsetterでセットして変数を取得し出力*/
 		System.out.println(animalBean.getAnimalName());
 		System.out.println(animalBean.getSpeed());
 		System.out.println(animalBean.getBodyLength());
--- Hello/src/java課題23/animalBean.java
+++ Hello/src/java課題23/animalBean.java
@@ -2,31 +2,37 @@ package java課題23;
 
 public class animalBean {
 		 
-	
+	/*それぞれのフィールドを定義*/
 	private String animalName;
 	private String speed;
 	private String bodyLength;
-
+	
+	/*getterメソッド*/
 	public String getAnimalName() {
 		return animalName;
 	}
-
+	
+	/*setterメソッド*/
 	public void setAnimalName(String animalName) {
 		this.animalName = animalName;
 	}
 	
+	/*getterメソッド*/
 	public String getSpeed() {
 		return speed;
 	}
-
+	
+	/*setterメソッド*/
 	public void setSpeed(String speed) {
 		this.speed = speed;
 	}
 	
+	/*getterメソッド*/
 	public String getBodyLength() {
 		return  bodyLength;
 	}
-
+	
+	/*setterメソッド*/
 	public void setBodyLength(String bodyLength) {
 		this.bodyLength = bodyLength;
 	}
